2db3e285c69f1f6aa5d012459d29ecc432f541ae
Jacqueline <100223579+delicatedoom@users.noreply.github.com>
2022-03-02 11:15:23 -0700

    Hope this is what It wants?

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout (aWord) {
+  console.log(aWord.toUpperCase( ));
+}
+function logWhisper (aWord) {
+  console.log(aWord.toLowerCase( ));
+}
+function sayHiToHeadphonedRoommate (aWord) { 
+  var cantUnswer = "I can't hear you!";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return cantUnswer;}}
